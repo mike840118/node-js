a16d8d8e483bc9d7a760e6e799074df6687c1a7a
陳哲宇 <milk840118@yahoo.com.tw>
2020-05-19 16:22:53 +0800

    0519 16:22

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 let express = require('express');
 
 let app = express();
+app.set('view engine', 'ejs');
+//middleware
+app.use(express.urlencoded({extended : false}));
+//或者 
+app.use(express.json());
 
-app.get('/',function(req ,res){
-    res.send('Hello World!!');
+app.get('/json' ,function(req,res){
+    const data = require(__dirname + '/../data/sales');
+    // res.json(data[2]);
+    res.render('sales', {
+       sales : data
+    });
+});
+
+app.get('/', function (req, res) {
+    // res.render('sales', {name: 'Shinder'});
+    res.send("ok")
+})
+
+ 
+app.get('/try-post-from', (req,res) =>{
+    // req.body.haha("shine")
+    res.render('try-post-from')
+});
+
+app.get('/try-get', (req,res) =>{
+    // req.body.haha("shine")
+    res.render('try-post-from') 
+    // req.body.content=res.get("message")
+});
+
+
+app.post('/try-post-from', (req,res) =>{
+    
+    res.render('try-post-from',req.body)
+});
+
+app.get('/try-qs',  (req, res)=> {
+   
+    res.json(req.query);
 })
 
 app.get('/panding',function(req ,res){
     
 })
 app.get('/start',function(req ,res){
-  req.send("/start")
+  res.send("start")
 })
 // app.get('/a.html',function(req ,res){
 //     res.send('Hello World!!!!!!');
