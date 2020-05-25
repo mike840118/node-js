67b6f9e6a4c06bd575b1e552deedfe97324bfb91
陳哲宇 <milk840118@yahoo.com.tw>
2020-05-25 22:53:19 +0800

    2020 0525 22:53 CRUD

--- src/address_book.js
+++ src/address_book.js
@@ -1,14 +1,122 @@
 const express = require('express');
+const moment = require('moment-timezone');
+const upload = require(__dirname + '/upload-module');
 const db = require(__dirname + '/db_connect2');
 
 const router = express.Router();
 
-router.get('/list', (req, res)=>{
-
-    db.query("SELECT * FROM address_book LIMIT 10")
-        .then(([rows])=>{
-            res.render('address-book/list', { rows });
+router.get('/', (req, res)=>{
+    res.redirect(req.baseUrl + '/list');
+});
+router.get('/del/:sid', (req, res)=>{
+    let referer = req.get('Referer'); // 從哪裡來
+    const sql = "DELETE FROM `address_book` WHERE sid=?";
+    db.query(sql, [req.params.sid])
+        .then(([r])=>{
+            if(referer){
+                res.redirect(referer)//從哪裡去
+            } else {
+                res.redirect('/address-book/list')//回主畫面
+            }
         })
+})
+router.get('/edit/:sid', (req, res)=>{
+    const sql = "SELECT * FROM address_book WHERE sid=?";
+    db.query(sql, [req.params.sid])
+        .then(([r])=>{
+            if(r && r.length){
+                r[0].birthday = moment(r[0].birthday).format('YYYY-MM-DD');
+                res.render('address-book/edit', r[0])
+            } else {
+                res.redirect('/address-book/list')
+            }
+        })
+})
+
+router.post('/edit', upload.none(), (req, res)=>{
+    const output = {
+        success: false,
+        body: req.body
+    }
+    let sid = parseInt(req.body.sid);
+    if(! sid){
+        output.error = '沒有主鍵';
+        return res.json(output);
+    }
+    const sql = "UPDATE `address_book` SET ? WHERE sid=?";
+    delete req.body.sid;
+    db.query(sql, [req.body, sid])
+        .then(([r])=>{
+            output.results = r;
+            if(r.affectedRows && r.changedRows){
+                output.success = true;
+            }
+            res.json(output);
+        })
+})
+
+
+router.get('/add', (req, res)=>{
+    res.render('address-book/add');
+})
+router.post('/add', upload.none(), (req, res)=>{
+    const output = {
+        success: false
+    }
+    // TODO: 檢查欄位
+
+    const sql = "INSERT INTO address_book set ?, created_at=NOW()";
+    //req.body.created_at = new Date();
+
+    db.query(sql, [req.body])
+        .then(([r])=>{
+            output.results = r;
+            if(r.affectedRows && r.insertId){
+                output.success = true;
+            }
+            res.json(output);
+        })
+    //res.json(req.body);
+})
+
+const getDataList = async (req)=>{
+    const perPage = 5;
+    let page = parseInt(req.params.page) || 1;
+
+    const output = {
+        // page: page,
+        perPage: perPage,
+        totalRows: 0, // 總共有幾筆資料
+        totalPages: 0, //總共有幾頁
+        rows: []
+    }
+    const [r1] = await db.query("SELECT COUNT(1) num FROM address_book");
+    output.totalRows = r1[0].num;
+    output.totalPages = Math.ceil(output.totalRows/perPage);
+    if(page < 1) page=1;
+    if(page > output.totalPages) page=output.totalPages;
+    if( output.totalPages===0) page=0;
+    output.page = page;
+
+    if(! output.page){
+        return output;
+    }
+    const sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(page-1)*perPage}, ${perPage}`
+    const [r2] = await db.query(sql);
+    if(r2) output.rows = r2;
+    for(let i of r2){
+        i.birthday = moment(i.birthday).format('YYYY-MM-DD');
+    }
+    return output;
+};
+
+router.get('/list/:page?', async (req, res)=>{
+    const output = await getDataList(req);
+    res.render('address-book/list', output);
+})
+router.get('/api/list/:page?', async (req, res)=>{
+    const output = await getDataList(req);
+    res.json(output);
 })
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-let express = require('express');
+const express = require('express');
 // const multer = require('multer');
+const session = require('express-session');
+const moment = require('moment-timezone');
+
+const fs = require('fs');
 // const upload = multer({dest: 'tmp_uploads/'})
 const upload = require(__dirname + '/upload-module');
 
 
-const moment = require('moment-timezone');
-const fs = require('fs');
 const app = express();
 
-
 app.set('view engine', 'ejs');
-//middleware
-app.use(express.urlencoded({extended : false}));
-const session = require ('express-session');
+
+// top-level middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(session({
-    // 新用戶沒有使用到 session 物件時不會建立 session 和發送 cookie
-    saveUninitialized:false,
-    resave:false,// 沒變更內容是否強制回存
-    secret:'121huaijnkslajjfkadljgal;kjdg;md;c',//加密文字
+    saveUninitialized: false,
+    resave: false,
+    secret: 'dolkidf;lalsdfjls',
     cookie:{
-        maxAge:1200000,//20分鐘，毫秒
+        maxAge: 1200000
     }
 }));
-//或者 
-app.use(express.json());
-//另外一個middleware
-app.use((req,res ,next)=>{
-    res.locals.memberData={
-        name:'shin',
-        action:'edit'
-    }
+app.use((req, res, next)=>{
+
+    res.locals.sess = req.session || {};
+
+    // res.locals.customData = {
+    //     name: 'shin',
+    //     action: 'edit'
+    // }
     next();
 });
-//登入的middleware
-app.use((req,res,next)=>{
-    res.locals.sess=req.session ||{}
-    next();
-})
+
+
+app.get('/', (req, res)=>{
+    res.render('main', {name: 'Shin', pageTitle: '小新的網站'});
+});
 
 app.get('/sales-json', (req, res)=>{
     const sales = require(__dirname + '/../data/sales');
@@ -44,94 +45,20 @@ app.get('/sales-json', (req, res)=>{
     res.render('sales-json', { sales })
 });
 
-app.get('/', (req, res)=>{
-    
-    res.render('main', {name: 'Shin'});
-   
-});
-app.get('/try-qs',  (req, res)=> {
-   
+app.get('/try-qs', (req, res)=>{
+    console.log(req)
     res.json(req.query);
 })
-app.get('/try-upload', (req, res)=>{
-    res.render('try-upload');
+
+app.get('/try-post-form', (req, res)=>{
+    res.render('try-post-form', {pageTitle: '測試表單'})
 })
 
-// app.post('/try-upload', upload.single('avatar'), (req, res)=>{
-    
-//     const output = {
-//         success: false,
-//         uploadedImg: '',
-//         nickname: '',
-//         errorMsg: ''
-//     }
-//     output.nickname = req.body.nickname || '';
-//     if(req.file && req.file.originalname){
-//         switch(req.file.mimetype){
-//             case 'image/png':
-//             case 'image/jpeg':
-//                 fs.rename(req.file.path, './public/img/'+ req.file.originalname, error=>{
-//                     if(!error){
-//                         output.success = true;
-//                         output.uploadedImg = '/img/' + req.file.originalname;
-//                     }
-//                     res.render('try-upload', output);
-//                 })
-//                 break;
-//             default:
-//                 fs.unlink(req.file.path, error=>{
-//                     output.errorMsg = '檔案類型錯誤'
-//                     res.render('try-upload', output);
-//                 })
-//         }
-//     }
-// });
-//  組合的方式----
-// const func =
-//  (req, res)=>{
-//     res.json({
-//         filename: req.file.filename,
-//         body: req.body
-//     });
-// }
-app.post('/try-upload2', upload.single('avatar'), (req, res)=>{
-    res.json({
-        filename: req.file.filename,
-        body: req.body
-        
-    });
+app.post('/try-post-form', (req, res)=>{
+    // req.body.haha = 'shin';
+    res.locals.pageTitle = '測試表單- posted'
+    res.render('try-post-form', req.body)
 })
-
-// app.get('/my-params1/:action/:id', (req, res)=>{
-//      res.json(req.params);
-// });
-
-// app.get('/my-params1/:action?/:id?', (req, res)=>{
-//     res.json(req.params);
-// });
-
-// app.get('/my-params1/*/*?', (req, res)=>{
-//      res.json(req.params);
-// });
-
-app.get(/^\/mobile\/09\d{2}-?\d{3}-?\d{3}$/, (req, res) =>{
-    let url = req.url.slice(8).split('?')[0];
-    url= url.split('-').join('');
-    res.send('Mobile : ' + url);
-    // req.body.haha("shine")
-    
-});
-// const admin2Router = require(__dirname + '/admins/admin2');
-app.use('/my/', require(__dirname + '/admins/admin2'));
-
-app.get('/try-post-from', (req,res) =>{
-    // req.body.haha("shine")
-    res.render('try-post-from')
-});
-app.post('/try-post-from', (req,res) =>{
-    
-    res.render('try-post-from',req.body)
-});
 
 app.post('/try-json-post', (req, res)=>{
     req.body.haha = 'shin';
@@ -140,15 +67,81 @@ app.post('/try-json-post', (req, res)=>{
     res.json(req.body);
 })
 
-app.get('/pending', (req, res)=>{
-
-});
+app.get('/pending', (req, res)=>{});
 app.get('/ok', (req, res)=>{
     res.send('ok');
 });
-app.get('/login',(req,res)=>{
-res.render('login')
+
+app.get('/try-upload', (req, res)=>{
+    res.render('try-upload');
 })
+/*
+app.post('/try-upload', upload.single('avatar'), (req, res)=>{
+    const output = {
+        success: false,
+        uploadedImg: '',
+        nickname: '',
+        errorMsg: ''
+    }
+    output.nickname = req.body.nickname || '';
+    if(req.file && req.file.originalname){
+        switch(req.file.mimetype){
+            case 'image/png':
+            case 'image/jpeg':
+                fs.rename(req.file.path, './public/img/'+ req.file.originalname, error=>{
+                    if(!error){
+                        output.success = true;
+                        output.uploadedImg = '/img/' + req.file.originalname;
+                    }
+                    res.render('try-upload', output);
+                })
+                break;
+            default:
+                fs.unlink(req.file.path, error=>{
+                    output.errorMsg = '檔案類型錯誤'
+                    res.render('try-upload', output);
+                })
+        }
+    }
+});
+*/
+
+app.post('/try-upload2', upload.single('avatar'), (req, res)=>{
+    res.json({
+        filename: req.file.filename,
+        body: req.body
+    });
+})
+
+app.get('/my-params1/:action?/:id?', (req, res)=>{
+    res.json(req.params)
+})
+
+app.get(/^\/mobile\/09\d{2}-?\d{3}-?\d{3}$/, (req, res)=>{
+    let url = req.url.slice(8).split('?')[0];
+    url = url.split('-').join('');
+
+    res.send('Mobile: ' + url)
+})
+
+// const admin2Router = require(__dirname + '/admins/admin2');
+// app.use(admin2Router);
+
+app.use('/my', require(__dirname + '/admins/admin2') );
+
+app.get('/try-session', (req, res)=>{
+    req.session.my_var = req.session.my_var || 0;
+    req.session.my_var++;
+
+    res.json({
+        my_var: req.session.my_var,
+        session: req.session
+    })
+})
+
+app.get('/login', (req, res)=> {
+    res.render('login');
+});
 app.post('/login', upload.none(), (req, res)=> {
     const users = {
         'shin' : {
@@ -178,39 +171,30 @@ app.post('/login', upload.none(), (req, res)=> {
     res.json(output);
 });
 
-app.get('/logout',(req,res)=>{
+app.get('/logout', (req, res)=> {
     delete req.session.user;
     res.redirect('/login');
-    })
-// app.get('/a.html', (req, res)=>{
-//     res.send('from route');
-// });
-// 時間格式化輸出
-app.get('/try-moment',(req,res)=>{
+});
+
+app.get('/try-moment', (req, res)=> {
     const fm = 'YYYY-MM-DD HH:mm:ss';
     const m1 = moment(new Date());
     const m2 = moment(req.session.cookie.expires);
-    const m3 = moment('2019-01-10')
+    const m3 = moment('2019-01-02')
+
     res.json({
-        m1:m1.format(fm),
-        m2:m2.format(fm),
-        m3:m3.format(fm),
-        m4:m1.tz('Europe/London').format(fm),
-        m5:m2.tz('Europe/London').format(fm),
-        m6:m3.tz('Europe/London').format(fm),
+        m1: m1.format(fm),
+        m2: m2.format(fm),
+        m3: m3.format(fm),
+        m1a: m1.tz('Europe/London').format(fm),
+        m2a: m2.tz('Europe/London').format(fm),
+        m3a: m3.tz('Europe/London').format(fm),
     })
+
 });
 
 app.use('/address-book', require(__dirname+'/address_book'));
 
-app.get('/try-session',(req,res)=>{
-    req.session.my_var=req.session.my_var||0;
-    req.session.my_var++
-    res.json({
-        my_var:req.session.my_var,
-        session:req.session
-    })
-})
 
 app.use(express.static('public'));
 
